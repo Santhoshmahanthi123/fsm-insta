4b1ad31617626b151cc2299bf22a66fc3d0e988c
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-12 16:43:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`server started on port:${port}`);
+});
