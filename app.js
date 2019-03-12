51e8dfd846532d047f997ca1a5e6ff5047974f26
Santhoshmahanthi123 <singumahanthisanthosh@gmail.com>
2019-03-12 17:13:15 +0530

    privacy policy added

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.render("index");
 });
+app.get("/privacy", (req, res) => {
+  res.render("privacy");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
 
 app.listen(port, () => {
   console.log(`server started on port:${port}`);
